0427373456a19af59cdf87f2f74cf94e229f1d5b
alyssabartuch <alyssabartuch@gmail.com>
2021-02-18 16:10:10 -0600

    committing frontend app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Tactic from "./types/Tactic";
+import TacticBoard from "./components/TacticBoard";
+import axios from "axios";
+import { getSideToPlayFromFen } from "./utils";
+import React from "react";
+
+async function fetchTactic() {
+  let tacticData;
+
+  try {
+    const response = await axios.get("https://chess-proxy.herokuapp.com/");
+    tacticData = await response.data;
+    console.log("Tactic Information", tacticData);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {
+    id: tacticData.id,
+    fen: tacticData.fenBefore,
+    blunderMove: tacticData.blunderMove,
+    solution: tacticData.forcedLine,
+  };
+}
 
 function App() {
+  const [tactics, setTactics] = useState<Tactic[]>([]);
+  const [hint, setHint] = useState<
+    "sideToPlay" | "incorrect" | "correct" | "solved"
+  >("sideToPlay");
+
+  const loadTactic = async () => {
+    try {
+      const newTactic = await fetchTactic();
+      setTactics((it) => it.concat(newTactic));
+      setHint("sideToPlay");
+    } catch (error) {
+      console.log("Error loading tactic", { error });
+    }
+  };
+
+  useEffect(() => {
+    loadTactic();
+  }, []);
+
+  if (tactics.length === 0) {
+    return <div className="overlay-loading">Loading...</div>;
+  }
+
+  const tactic = tactics[0];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-center app-container">
+      <h1>Chess Tactics Trainer</h1>
+      <div className="hint-container">
+        {hint === "sideToPlay" && (
+          <div className="tactic-hint">
+            {getSideToPlayFromFen(tactic.fen) === "b" ? "White" : "Black"} to
+            Move
+          </div>
+        )}
+        {hint === "correct" && (
+          <div className="tactic-hint tactic-hint-success">Correct!</div>
+        )}
+
+        {hint === "incorrect" && (
+          <div className="tactic-hint tactic-hint-error">Incorrect!</div>
+        )}
+
+        {hint === "solved" && (
+          <div className="tactic-hint tactic-hint-solved">Solved!</div>
+        )}
+      </div>
+
+      <TacticBoard
+        key={tactic.id}
+        tactic={tactic}
+        onCorrect={() => {
+          setHint("correct");
+          setTimeout(() => setHint("sideToPlay"), 3000);
+        }}
+        onIncorrect={() => {
+          setHint("incorrect");
+          setTimeout(() => setHint("sideToPlay"), 3000);
+        }}
+        onSolve={() => {
+          setHint("solved");
+          setTactics((it) => it.slice(1));
+          loadTactic();
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/TacticBoard.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import "../components/TacticBoard.css";
+import { ShortMove } from "chess.js";
+import Chessboard from "chessboardjsx";
+import Tactic from "../types/Tactic";
+import { getSideToPlayFromFen, makeMove, validateMove } from "../utils";
+
+interface Props {
+  tactic: Tactic;
+  onIncorrect: () => void;
+  onCorrect: () => void;
+  onSolve: () => void;
+}
+
+const TacticBoard: React.FC<Props> = ({
+  tactic,
+  onIncorrect,
+  onCorrect,
+  onSolve,
+}) => {
+  const [fen, setFen] = useState(tactic.fen);
+  const [solution, setSolution] = useState(tactic.solution);
+  const [hint, setHint] = useState(tactic.solution);
+  const [isHintVisible, setIsHintVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const next = makeMove(tactic.fen, tactic.blunderMove);
+      if (next) {
+        setFen(next.fen);
+      }
+    }, 100);
+  }, [tactic]);
+
+  const handleMove = (move: ShortMove) => {
+    const next = validateMove(fen, move, solution);
+
+    if (next) {
+      setFen(next.fen);
+      setSolution(next.solution);
+
+      if (next.solution.length > 0) {
+        onCorrect();
+
+        const autoNext = validateMove(
+          next.fen,
+          next.solution[0],
+          next.solution
+        );
+
+        if (autoNext) {
+          setHint(autoNext.solution);
+          setFen(autoNext.fen);
+          setSolution(autoNext.solution);
+        }
+      } else {
+        onSolve();
+      }
+    } else {
+      onIncorrect();
+    }
+  };
+
+  const toggleHintVisibility = () => {
+    setIsHintVisible(!isHintVisible);
+  };
+
+  return (
+    <div>
+      <Chessboard
+        transitionDuration={200}
+        position={fen}
+        width={600}
+        orientation={
+          getSideToPlayFromFen(tactic.fen) === "b" ? "white" : "black"
+        }
+        onDrop={(move) =>
+          handleMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            promotion: "q",
+          })
+        }
+      />
+      <div className="hint-container">
+        <div className="hint-btn" onClick={() => toggleHintVisibility()}>
+          Hint Move
+        </div>
+        {isHintVisible && (
+          <div className="hint-content">
+            <h3>{hint[0]}</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TacticBoard;
